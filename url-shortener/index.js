4b47358544c467eb6611e2e3760089b049caa6f4
Virender Parasariya <109868915+virend3rp@users.noreply.github.com>
2025-09-01 20:58:40 +0530

    Update index.js

--- url-shortener/index.js
+++ url-shortener/index.js
@@ -24,7 +24,7 @@ redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://your-vercel-frontend-url.vercel.app'],
+    origin: ['http://localhost:5173', 'https://url-shortner-jo0wiy6ek-virend3rps-projects.vercel.app'],
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
